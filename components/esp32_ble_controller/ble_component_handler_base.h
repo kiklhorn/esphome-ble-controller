22075dc2e0d12360c4ce78e5fd78d93ce159fd52
kiklhorn <6151364+kiklhorn@users.noreply.github.com>
2022-10-24 17:30:21 +0200

    Update ble_component_handler_base.h

--- components/esp32_ble_controller/ble_component_handler_base.h
+++ components/esp32_ble_controller/ble_component_handler_base.h
@@ -2,9 +2,9 @@
 
 #include <string>
 
-#include <Arduino.h>
-//#include <BLEServer.h>
-//#include <BLECharacteristic.h>
+//#include <Arduino.h>
+#include <BLEServer.h>
+#include <BLECharacteristic.h>
 
 #include "esphome/core/entity_base.h"
 #include "esphome/core/controller.h"
